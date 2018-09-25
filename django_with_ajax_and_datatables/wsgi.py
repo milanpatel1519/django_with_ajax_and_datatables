88dd408ae34f039938c3bbd015d8644dd7312f35
Milan Patel <milan.patel@drcsystems.com>
2018-09-25 15:53:52 +0530

    [ADD] initial project

---
+++ django_with_ajax_and_datatables/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for django_with_ajax_and_datatables project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_with_ajax_and_datatables.settings')
+
+application = get_wsgi_application()
